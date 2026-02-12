558046b0dec8769b108b1a15b3aa1af011738f77
itc-n25005 <n25005@std.it-college.ac.jp>
2026-02-12 12:41:05 +0900

    今日の経過

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,46 @@
-import Image from "next/image";
+"use client";
+
 import styles from "./page.module.css";
+
+type Task = {
+  id: number;
+  text: string;
+  time: string;
+  tag: {
+    name: string;
+    color: string;
+  };
+  done: boolean;
+};
+
+const tasks: Task[] = [
+  {
+    id: 1,
+    text: "オンライン説明会",
+    time: "2026-02-12T10:00:00",
+    tag: { name: "NextEvent", color: "blue" },
+    done: false,
+  },
+];
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="title">Welcome to the Event Reminder</h1>
-    </div>
+    <main>
+      <h1 className={styles.title}>リマインダーアプリへようこそ！</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <span
+              className={styles.tag}
+              style={{ backgroundColor: task.tag.color }}
+            >
+              {task.tag.name}
+            </span>
+            <span className={styles.text}>{task.text}</span>
+            <span className={styles.time}>{task.time}</span>
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 }
