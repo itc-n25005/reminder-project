baf07da75ccb05ce60362b1f5ddbf6cdb27f0cde
itc-n25005 <n25005@std.it-college.ac.jp>
2026-02-15 20:45:28 +0900

    途中経過

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
 "use client";
 
 import styles from "./page.module.css";
+import ButtonLink from "./ButtonLink/page";
 
 type Task = {
+  id: number;
+  text: string;
+  time: string;
+  tag: {
+    name: string;
+    color: string;
+  };
+  done: boolean;
+};
+
+type Todo = {
   id: number;
   text: string;
   time: string;
@@ -23,10 +35,21 @@ const tasks: Task[] = [
   },
 ];
 
+const todos: Todo[] = [
+  {
+    id: 1,
+    text: "起床",
+    time: "2026-02-12T07:00:00",
+    tag: { name: "Morning", color: "orange" },
+    done: false,
+  },
+];
+
 export default function Home() {
   return (
     <main>
       <h1 className={styles.title}>リマインダーアプリへようこそ！</h1>
+      <h2 className={styles.subtitle}>今日の予定</h2>
       <ul>
         {tasks.map((task) => (
           <li key={task.id}>
@@ -41,6 +64,24 @@ export default function Home() {
           </li>
         ))}
       </ul>
+      <h3 className={styles.subtitle}>定期リマインダー</h3>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span
+              className={styles.tag}
+              style={{ backgroundColor: todo.tag.color }}
+            >
+              {todo.tag.name}
+            </span>
+            <span className={styles.text}>{todo.text}</span>
+            <span className={styles.time}>{todo.time}</span>
+          </li>
+        ))}
+      </ul>
+      <div className={styles.reminderLink}>
+        <ButtonLink href="/reminder">リマインダーを確認する</ButtonLink>
+      </div>
     </main>
   );
 }
